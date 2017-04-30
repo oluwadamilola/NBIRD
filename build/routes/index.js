f8d1c86eaf0ed1074266439e6cef4abd94cc5495
Akinmade Bond <lovedammy77@yahoo.com>
2017-04-30 14:51:36 +0100

    Do not track build directory.
    Files in the folder are automatically compiled by TypeScript.
    
    Removed and deleted alreadt tracked files by source control.

--- build/routes/index.js
+++ build/routes/index.js
@@ -2,16 +2,13 @@
 exports.__esModule = true;
 var express = require("express");
 var router = express.Router();
+var birth_1 = require("../controllers/birth");
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
-router.get('/birth', function (req, res, next) {
-    res.render('birthreg');
-});
-router.post('/birth', function (req, res, next) {
-    res.send(req.body);
-});
+router.get('/birth', birth_1.getBirthForm);
+router.post('/birth', birth_1.registerBirth);
 router.get('/death', function (req, res, next) {
     res.render('deathreg');
 });
