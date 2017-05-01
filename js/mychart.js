0c71d0ad42ea7471c33aa8767002fdd9667cc78c
Oluwadamilola Olukoju <dammyolukoju@gmail.com>
2017-05-01 15:50:03 +0100

    changes made so far

--- js/mychart.js
+++ js/mychart.js
@@ -1,27 +1,19 @@
-
-// Using CommonJS
-var Chart = require('chart.js')
-var myChart = new Chart({myChat});
-
-var ctx = document.getElementById("myChart");
-var ctx = document.getElementById("myChart").getContext("2d");
-var ctx = $("#myChart");
-var ctx = "myChart";
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'BIRTH RATES',
+            data: [111, 1, 1, 1, 1, 0,0,0,0],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 255, 240, 0.2)',
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
